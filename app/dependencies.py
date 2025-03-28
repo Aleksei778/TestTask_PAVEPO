70a8f185806a01459572aa8e8d8627f9483ea325
Alexey Parkhomenko <aleksejparhomenko14192@gmail.com>
2025-03-28 22:26:24 +0300

    add get_current_user to dependencies

---
+++ app/dependencies.py
@@ -0,0 +1,50 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from database import get_db_conn
+from models import User
+
+security = HTTPBearer()
+
+async def get_current_user(
+        credentials: HTTPBearer = Depends(security),
+        db: AsyncSession = Depends(get_db_conn)
+):
+    db_manager = DBManager(session=db)
+
+    try:
+        if not credentials or not credentials.credentials:
+            raise HTTPException(
+                status_code=401,
+                detail="No auth data"
+            )
+        
+        payload = await verify_token(token=credentials.credentials)
+
+        user_id = payload.get("sub")
+
+        if user_id is None:
+            raise HTTPException(
+                status_code=401,
+                detail="No user data"
+            )
+        
+    except:
+        raise HTTPException(
+                status_code=401,
+                detail="Could not validate creds"
+            )
+    
+    user = await db_manager.get_user_from_db(user_id)
+
+    if user is None:
+        raise HTTPException(
+            status_code=401,
+            detail="No such user"
+        )
+    
+    return user
+
+
+
--- config.py
+++ config.py
@@ -8,3 +8,6 @@ DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
+
+YANDEX_CLIENT_ID = os.getenv("YANDEX_CLIENT_ID")
+YANDEX_SECRET = os.getenv("YANDEX_SECRET")
